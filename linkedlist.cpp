eed86a70b348581ccf0b1bae2196c7c2f4994e5b
Sridhar Venkat <svenkat@MacBook-Pro-4.local>
2019-05-20 13:19:52 -0400

    Initial commit

---
+++ linkedlist.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class MyNode {
+    public:
+        int value;
+        MyNode *next;
+};
+
+int main() {
+    MyNode *head = new MyNode();
+    MyNode *body = new MyNode();
+    MyNode *tail = new MyNode();
+
+    head->value = 77;
+    body->value = 88;
+    tail->value = 99;
+
+    head->next = body;
+    body->next = tail;
+
+    MyNode *temp = head;
+
+    while(temp) {
+        cout << "Value is : " << temp->value << endl;
+        temp = temp->next;
+    }
+
+    return 0;
+}
